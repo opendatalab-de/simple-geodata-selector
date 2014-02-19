8588d89f767bfce1a46ca157d9ee63c7a1273490
Felix Ebert <felix.ebert@if-core.de>
2014-02-20 00:54:42 +0100

    jsonToTable: show nested objects

--- src/js/jsonToTable.js
+++ src/js/jsonToTable.js
@@ -1,13 +1,18 @@
 (function(sgs) {
 	'use strict';
 
-	var jsonToTable = function(jsonObject) {
-		var html = '<div class="property-list"><table class="table table-bordered"><tbody>';
+	var jsonToTable = function(jsonObject, nested) {
+		var html = (nested) ? '' : '<div class="property-list">';
+		html += '<table class="table table-bordered"><tbody>';
 		for ( var property in jsonObject) {
 			var value = jsonObject[property];
+			if (!nested && typeof value == 'object') {
+				value = jsonToTable(value, true);
+			}
 			html += '<tr><th>' + property + '</th><td>' + value + '</td></tr>';
 		}
-		html += '</tbody></table></div>';
+		html += '</tbody></table>';
+		html += (nested) ? '' : '</div>';
 		return html;
 	};
 
