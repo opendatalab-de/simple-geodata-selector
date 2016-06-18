fa2cc8cfa0da69e672cfbcfadb05d45037400c92
Felix Ebert <felix.ebert@if-core.de>
2016-06-18 11:22:19 +0200

    destatis: sum data for export of counties and states

--- src/js/destatis.js
+++ src/js/destatis.js
@@ -1,23 +1,48 @@
 'use strict';
 
-(function(sgs, $) {
-    var enrichGeojson = function(geojson, destatisData) {
-        geojson.features.forEach(function(feature) {
-            feature.properties.destatis = destatisData[feature.properties.RS];
+(function (sgs, $) {
+    var superiorLevelSumKeys = ['population', 'population_m', 'population_w'];
+    var superiorLevelResultInitialValue = {};
+    superiorLevelSumKeys.forEach(function (key) {
+        superiorLevelResultInitialValue[key] = 0;
+    });
+
+    var enrichGeojson = function (geojson, destatisData) {
+        geojson.features.forEach(function (feature) {
+            if (feature.properties.RS.length > 5) {
+                // commune
+                feature.properties.destatis = destatisData[feature.properties.RS];
+            }
+            else {
+                // administrative level above commune (county, state)
+                feature.properties.destatis = Object.keys(destatisData)
+                    .filter(function (key) {
+                        return key.indexOf(feature.properties.RS) === 0;
+                    })
+                    .map(function (key) {
+                        return destatisData[key];
+                    })
+                    .reduce(function (result, currentRow) {
+                        superiorLevelSumKeys.forEach(function (key) {
+                            result[key] += currentRow[key];
+                        });
+                        return result;
+                    }, Object.assign({}, superiorLevelResultInitialValue));
+            }
         });
     };
-    
-    var enrich = function(geojson, finalCallback) {
+
+    var enrich = function (geojson, finalCallback) {
         $.ajax({
             dataType: 'json',
             url: 'data/destatis.json',
-            success: function(data) {
+            success: function (data) {
                 enrichGeojson(geojson, data);
                 finalCallback();
             }
         });
     };
-    
+
     sgs.destatis = {
         'enrich': enrich
     };
